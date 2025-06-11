18aced6b2448f8f1203e0cfdb99ca1f933f45c40
lsmasters <lmasters@algomau.ca>
2025-06-11 19:26:24 +0000

    Login page:  unsuccessful attempts added

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -1,12 +1,16 @@
-<?php require_once 'app/views/templates/headerPublic.php'?>
+<?php //require_once 'app/views/templates/headerPublic.php'
+	if(isset($_SESSION['failedAuth'])) { 
+		echo "This is unsuccessful login attempt # " . $_SESSION['failedAuth']; 
+		echo '<p style="color:red">PLEASE TRY AGAIN!  </p>';
+	}
+	else{
+		echo '<p>Please login to continue. </p>';
+  }
+  
+?>
 <main role="main" class="container">
-    <div class="page-header" id="banner">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1>You are not logged in</h1>
-            </div>
-        </div>
-    </div>
+<h1>Welcome to Assigment 3</h1>
+<br>
 
 <div class="row">
     <div class="col-sm-auto">
@@ -14,7 +18,7 @@
 		<fieldset>
 			<div class="form-group">
 				<label for="username">Username</label>
-				<input required type="text" class="form-control" name="username">
+				<input required type="text" class="form-control" name="username" autofocus>
 			</div>
 			<div class="form-group">
 				<label for="password">Password</label>
@@ -24,6 +28,7 @@
 		    <button type="submit" class="btn btn-primary">Login</button>
 		</fieldset>
 		</form> 
+		<p>  <a href='/create_user.php'><br><br>Click here to CREATE a new account </a></p>
 	</div>
 </div>
     <?php require_once 'app/views/templates/footer.php' ?>
